cebbab85f383fd7aecf00ca137d0c408ae846cd3
wkrzywdzinski <krzywdzinski.wojciech@gmail.com>
2018-11-13 19:14:55 +0100

    crime on coding

--- db.js
+++ db.js
@@ -1,72 +1,99 @@
-const spicedPg = require('spiced-pg');
-var bcrypt = require('bcryptjs');
-var bcrypt = require('bcryptjs');
+const spicedPg = require("spiced-pg");
+var bcrypt = require("bcryptjs");
+var bcrypt = require("bcryptjs");
 
-
-const db = spicedPg(
-    `postgres:postgres:anneanneanne@localhost:5432/petition`
-);
+const db = spicedPg(`postgres:postgres:anneanneanne@localhost:5432/petition`);
 
 exports.createsignature = function(userID, signature) {
-    return db.query(
-        `INSERT INTO signatures (userID, signature)
+  return db.query(
+    `INSERT INTO signatures (userID, signature)
         VALUES ($1, $2)
         RETURNING id`,
-        [userID || null, signature || null]
-    )
-}
-exports.getsignature = (userID) => {
-    return db.query(
-        `SELECT signature
+    [userID || null, signature || null]
+  );
+};
+exports.getsignature = userID => {
+  return db.query(
+    `SELECT signature
         FROM signatures
         WHERE userID = $1`,
-        [userID]
-    );
+    [userID]
+  );
 };
 exports.createuser = function(name, lastname, email, password) {
-    return db.query(
-        `INSERT INTO usersdata (name, lastname, email, password)
+  return db.query(
+    `INSERT INTO usersdata (name, lastname, email, password)
         VALUES ($1, $2, $3, $4)
         RETURNING id`,
-        [ name || null, lastname || null, email || null, password || null]
-    )
-}
-exports.getuser = (email) => {
-    return db.query(
-        `SELECT *
+    [name || null, lastname || null, email || null, password || null]
+  );
+};
+exports.insertinfo = function(userID, age, city, url) {
+  return db.query(
+    `INSERT INTO fullinfo (userID, age, city, url)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id`,
+    [userID || null, age || null, city || null, url || null]
+  );
+};
+exports.getuser = email => {
+  return db.query(
+    `SELECT *
         FROM usersdata
         WHERE email = $1`,
-        [email]
-    );
+    [email]
+  );
+};
+exports.getsigners = function() {
+  return db.query(
+    `SELECT name, lastname, age, city, url
+       FROM signatures
+       LEFT JOIN usersdata
+       ON usersdata.id = signatures.userID
+       LEFT JOIN fullinfo
+       ON fullinfo.userID = signatures.userID`
+  );
+};
+exports.checksignature = id => {
+  return db.query(
+    `SELECT *
+        FROM signatures
+        WHERE userID = $1`,
+    [id]
+  );
 };
 
 exports.hashPassword = function(plainTextPassword) {
-    return new Promise(function(resolve, reject) {
-        bcrypt.genSalt(function(err, salt) {
-            if (err) {
-                return reject(err);
-            }
-            bcrypt.hash(plainTextPassword, salt, function(err, hash) {
-                if (err) {
-                    return reject(err);
-                }
-                resolve(hash);
-                console.log(hash);
-            });
-        });
+  return new Promise(function(resolve, reject) {
+    bcrypt.genSalt(function(err, salt) {
+      if (err) {
+        return reject(err);
+      }
+      bcrypt.hash(plainTextPassword, salt, function(err, hash) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(hash);
+        console.log(hash);
+      });
     });
-}
+  });
+};
 
-
-exports.checkPassword = function(textEnteredInLoginForm, hashedPasswordFromDatabase) {
-  console.log("password check works");
-    return new Promise(function(resolve, reject) {
-        bcrypt.compare(textEnteredInLoginForm, hashedPasswordFromDatabase, function(err, doesMatch) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(doesMatch);
-            }
-        });
+exports.checkPassword = function(
+  textEnteredInLoginForm,
+  hashedPasswordFromDatabase
+) {
+  return new Promise(function(resolve, reject) {
+    bcrypt.compare(textEnteredInLoginForm, hashedPasswordFromDatabase, function(
+      err,
+      doesMatch
+    ) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(doesMatch);
+      }
     });
-}
+  });
+};
--- index.js
+++ index.js
@@ -1,69 +1,92 @@
 const express = require("express");
 const app = express();
-var cookieSession = require('cookie-session');
+var cookieSession = require("cookie-session");
 const hb = require("express-handlebars");
-const bodyParser = require('body-parser')
-const csurf = require('csurf');
-const db = require('./db')
+const bodyParser = require("body-parser");
+const csurf = require("csurf");
+const db = require("./db");
 app.engine("handlebars", hb());
 app.set("view engine", "handlebars");
 
 /////////////////////////middleware///////////////////
-app.use(cookieSession({
+app.use(
+  cookieSession({
     secret: `you dont wanna know.`,
     maxAge: 1000 * 60 * 60 * 24 * 14
-}));
-app.use(express.static("./public"))
+  })
+);
+app.use(express.static("./public"));
 app.use(
-   require("body-parser").urlencoded({
-       extended: false
-   })
+  require("body-parser").urlencoded({
+    extended: false
+  })
 );
 app.use(csurf());
 app.use(function(req, res, next) {
-    res.locals.csrfToken = req.csrfToken();
-    next();
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
-///////// redirect /////////
-//
-// app.use(function(req, res, next) {
-//   if (!req.session.id && req.url != "/signin" && !req.session.petitionsigned) {
-//     res.redirect("/signin")
-//   } else if (req.session.id && !req.session.petitionsigned) {
-//     res.redirect("/sign")
-// }
-//    else {
-//     next()}
-// })
 //////////////////get route////////////
 
 app.get("/", (req, res) => {
-res.redirect("/signin")
+  res.redirect("/signin");
 });
 
 app.get("/signin", (req, res) => {
-  res.render("signin", {
-    layout: "main"
-  });
+  if (req.session.id) {
+    res.redirect("/sign");
+  } else
+    res.render("signin", {
+      layout: "main"
+    });
 });
 
 app.get("/login", (req, res) => {
-  res.render("login", {
-    layout: "main"
-  });
+  if (req.session.id) {
+    res.redirect("/sign");
+  } else {
+    res.render("login", {
+      layout: "main"
+    });
+  }
 });
 
 app.get("/sign", (req, res) => {
-  res.render("sign", {
-    layout: "main"
-  });
+  if (req.session.signed) {
+    res.redirect("/signed");
+  } else if (!req.session.id) {
+    res.redirect("/signin");
+  } else {
+    res.render("sign", {
+      layout: "main"
+    });
+  }
 });
 
-
 app.get("/signed", (req, res) => {
-  res.render("signed", {
-    layout: "main"
-  });
+  if (!req.session.id) {
+    res.redirect("/signin");
+  } else if (req.session.signed) {
+    db.getsignature(req.session.id).then(function(results) {
+      req.session.signed = true;
+      console.log(results);
+      res.render("signed", {
+        layout: "main",
+        signature: results.rows[0].signature
+      });
+    });
+  } else if (req.session.id) {
+    res.redirect("sign");
+  }
+});
+app.get("/moreinfo", (req, res) => {
+  if (!req.session.id) {
+    res.redirect("/signin");
+  } else {
+    res.render("moreinfo", {
+      layout: "main"
+    });
+  }
 });
 app.get("/logout", (req, res) => {
   req.session = null;
@@ -72,72 +95,105 @@ app.get("/logout", (req, res) => {
     layout: "main"
   });
 });
-
+app.get("/signers", (req, res) => {
+  db.getsigners().then(function(results) {
+    res.render("signers", {
+      layout: "main",
+      list: results.rows
+    });
+  });
+});
 
 //////////////////// post route///////////////////
+
 app.post("/login", function(req, res) {
   db.getuser(req.body.email)
-  .then(function(results)
-  {
-    return db.checkPassword(req.body.password, results.rows[0].password).then(function(check) {
-        if(check){
-          req.session.id = results.rows[0].id
-          console.log(req.session)
-          res.redirect("/sign")
-        }
-          else {
-            throw err
+    .then(function(results) {
+      return db
+        .checkPassword(req.body.password, results.rows[0].password)
+        .then(function(check) {
+          if (check) {
+            req.session.id = results.rows[0].id;
+            console.log(req.session);
+            db.checksignature(req.session.id).then(function(results) {
+              if (results.rows.length) {
+                req.session.signed = true;
+                console.log("yes");
+                res.redirect("/sign");
+              } else {
+                console.log("no");
+                res.redirect("/sign");
+              }
+            });
+          } else {
+            throw err;
           }
-        })
-  })
-
-    .catch(function(err){
-      console.log(err)
+        });
+    })
+    .catch(function(err) {
+      console.log(err);
       res.render("login", {
         layout: "main",
-        error : err
-      })
-      })
-      })
-
+        error: err
+      });
+    });
+});
 
 app.post("/signin", function(req, res) {
   db.hashPassword(req.body.password)
-  .then(function(password){
-    db.createuser(req.body.firstname, req.body.lastname, req.body.email, password)
-      .then(function(results)
-        {
-        req.session.id = results.rows[0].id
-        console.log(req.session)
-        res.redirect("/sign")
-      })
-    }).catch(function(err){console.log(err)
+    .then(function(password) {
+      db.createuser(
+        req.body.firstname,
+        req.body.lastname,
+        req.body.email,
+        password
+      ).then(function(results) {
+        req.session.id = results.rows[0].id;
+        console.log(req.session);
+        res.redirect("/moreinfo");
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
       res.render("signin", {
         layout: "main",
-        error : err
-      })
-    })});
-
-
-  app.post("/sign", function(req, res) {
-        console.log(req.session.id);
-      db.createsignature(req.session.id, req.body.signaturedata).then(function(results) {
-      return db.getsignature(req.session.id)
-        }).then(function (results) {
-          console.log(results);
-          res.render("signed", {
-            layout: "main",
-            signature : results.rows[0].signature
-          })
-
-        }).catch(function(err){console.log(err)
-          res.render("sign", {
-            layout: "main",
-            error : err
-          })
-          })
+        error: err
       });
+    });
+});
 
+app.post("/sign", function(req, res) {
+  db.createsignature(req.session.id, req.body.signaturedata)
+    .then(function(results) {
+      return db.getsignature(req.session.id);
+    })
+    .then(function(results) {
+      req.session.signed = true;
+      console.log(results);
+      res.render("signed", {
+        layout: "main",
+        signature: results.rows[0].signature
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.render("sign", {
+        layout: "main",
+        error: err
+      });
+    });
+});
+app.post("/moreinfo", function(req, res) {
+  db.insertinfo(req.session.id, req.body.age, req.body.city, req.body.url)
+    .then(res.redirect("/sign"))
+    .catch(function(err) {
+      console.log(err);
+      res.render("login", {
+        layout: "main",
+        error: err
+      });
+    });
+});
 
 ////////////// server ///////////
 
