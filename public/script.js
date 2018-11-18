b98fbf0331fae5b2f8457d3afbd8c3686b923fae
wkrzywdzinski <krzywdzinski.wojciech@gmail.com>
2018-11-18 19:41:37 +0100

    crime on code

--- public/script.js
+++ public/script.js
@@ -1,16 +1,10 @@
 var input = $("input");
 var links = $("a");
-var catnip = $("#catnip");
 var deletebutton = $("#deletebutton");
 input.on("mousedown", function(e) {
   input.removeClass("selected");
   let inp = $(e.currentTarget);
   inp.addClass("selected");
-});
-catnip.on("mousedown", function(e) {
-  console.log("druck");
-  let inp = $(e.currentTarget);
-  $("#cover").css("zIndex", -100);
 });
 
 input.on("mouseover", function(e) {
